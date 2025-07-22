34a2e462e0f778562c3b9cd3f948356893d6b972
doinel1a <doinel1atanasiu@gmail.com>
2025-07-22 22:47:39 +0200

    refactor: replace shadcn/ui with hero-ui

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 
-import { Button } from './ui/button';
-import { Card, CardContent, CardHeader } from './ui/card';
+import { Button } from '@heroui/button';
+import { Card, CardBody, CardHeader } from '@heroui/card';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
     <Card className='border-border bg-secondary w-72 border shadow-md'>
-      <CardHeader>
+      <CardHeader className='flex flex-col'>
         <h1 className='text-2xl'>Vite React TypeScript</h1>
-        <h2 className='text-lg'>with shadcn/ui</h2>
+        <h2 className='text-lg'>with HeroUI</h2>
       </CardHeader>
-      <CardContent className='flex w-full items-center justify-between'>
+      <CardBody className='flex w-full flex-row items-center justify-between'>
         <Button
-          className='w-10 rounded-full'
+          color='primary'
+          className='rounded-full'
           data-testid='increase-count'
-          onClick={() => {
+          isIconOnly
+          onPress={() => {
             setCount((previousCount) => previousCount + 1);
           }}
         >
@@ -28,15 +30,17 @@ export default function Counter() {
         </h2>
 
         <Button
-          className='w-10 rounded-full'
+          color='primary'
+          className='rounded-full'
           data-testid='decrease-count'
-          onClick={() => {
+          isIconOnly
+          onPress={() => {
             setCount((previousCount) => previousCount - 1);
           }}
         >
           - 1
         </Button>
-      </CardContent>
+      </CardBody>
     </Card>
   );
 }
---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Button } from '@heroui/button';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/dropdown';
+import { useTheme } from '@heroui/use-theme';
+import { Laptop, MoonStar, Sun } from 'lucide-react';
+
+export default function ThemeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <Dropdown className='min-w-32'>
+      <DropdownTrigger>
+        <Button data-testid='theme-toggle' variant='bordered' isIconOnly>
+          <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+          <MoonStar className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+          <span className='sr-only'>Toggle theme</span>
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu data-testid='theme-dropdown-content'>
+        <DropdownItem
+          key='theme-light'
+          data-testid='theme-light'
+          onPress={() => {
+            setTheme('light');
+          }}
+        >
+          <div className='flex items-center'>
+            <Sun className='mr-2 h-[1.2rem] w-[1.2rem]' />
+            <span>Light</span>
+          </div>
+        </DropdownItem>
+        <DropdownItem
+          key='theme-dark'
+          data-testid='theme-dark'
+          onPress={() => {
+            setTheme('dark');
+          }}
+        >
+          <div className='flex items-center'>
+            <MoonStar className='mr-2 h-[1.2rem] w-[1.2rem]' />
+            <span>Dark</span>
+          </div>
+        </DropdownItem>
+        <DropdownItem
+          key='theme-system'
+          data-testid='theme-system'
+          onPress={() => {
+            setTheme('system');
+          }}
+        >
+          <div className='flex items-center'>
+            <Laptop className='mr-2 h-[1.2rem] w-[1.2rem]' />
+            <span>System</span>
+          </div>
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
