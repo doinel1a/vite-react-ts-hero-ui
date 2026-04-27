eac7f67533328b7c21f53a481e66ba88da2cc412
doinel1a <doinel1atanasiu@gmail.com>
2026-04-27 18:31:58 +0200

    fix: use correct vite react library

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 
 import path from 'node:path';
 
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 import config from './_config';
