cff11fddd051fb51450acc10c747cb70bd8333f8
doinel1a <doinel1atanasiu@gmail.com>
2026-04-27 18:46:59 +0200

    chore: enable react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import './styles/global.scss';
 import './styles/global.css';
 
+import { StrictMode } from 'react';
+
 import { createRoot } from 'react-dom/client';
 
 import App from './app';
@@ -8,4 +10,8 @@ import App from './app';
 const container = document.querySelector('#root');
 const root = createRoot(container as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
